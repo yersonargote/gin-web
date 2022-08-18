416a8018a3c273f56ceec80bcc075a2129702444
yersonargote <llalex.2045@gmail.com>
2022-08-18 10:47:49 -0500

    Add basic endopoint and manage from xml and json data

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Building Distributed Applications in Gin
 
 Following the book for learn to create apps in GO.
+
+### Note
+If you're running Go 1.16 and above, you need to disable Go modules via the
+```bash
+GO111MODULE=off option
+```
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/xml"
+
+	"github.com/gin-gonic/gin"
+)
+
+func IndexHandler(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
+		"message": "hello world",
+	})
+}
+
+func Greeting(ctx *gin.Context) {
+	name := ctx.Params.ByName("name")
+	ctx.JSON(200, gin.H{
+		"message": "hello " + name,
+	})
+}
+
+func InfoPerson(ctx *gin.Context) {
+	ctx.XML(200, Person{
+		FirstName: "Yerson",
+		LastName:  "Argote",
+	})
+}
+
+func InfoBreakfast(ctx *gin.Context) {
+	ctx.JSON(200, Breakfast{
+		Drink: "Coffe",
+		Fruit: "Banana",
+	})
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/", IndexHandler)
+	router.GET("/:name", Greeting)
+	router.GET("/person", InfoPerson)
+	router.GET("/breakfast", InfoBreakfast)
+	router.Run()
+}
+
+type Person struct {
+	XMLName   xml.Name `xml:"person"`
+	FirstName string   `xml:"firstName,attr"`
+	LastName  string   `xml:"lastName,attr"`
+}
+
+type Breakfast struct {
+	Drink string `json:"drink"`
+	Fruit string `json:"fruit"`
+}
