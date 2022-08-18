df51cfaeb3be9af29ac81f20aa3ddaca90616ae4
yersonargote <llalex.2045@gmail.com>
2022-08-18 17:11:32 -0500

    Change readme and failed try with swagger

--- main.go
+++ main.go
@@ -13,10 +13,13 @@ import (
 
 var recipes []Recipe
 
-func init() {
-	recipes = make([]Recipe, 0)
-	file, _ := ioutil.ReadFile("recipes.json")
-	_ = json.Unmarshal([]byte(file), &recipes)
+type Recipe struct {
+	ID           string    `json:"id"`
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
 }
 
 func ListRecipesHandler(ctx *gin.Context) {
@@ -107,6 +110,12 @@ func SearchRecipeHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, queryRecipes)
 }
 
+func init() {
+	recipes = make([]Recipe, 0)
+	file, _ := ioutil.ReadFile("recipes.json")
+	_ = json.Unmarshal([]byte(file), &recipes)
+}
+
 func main() {
 	router := gin.Default()
 	router.POST("/recipes", NewRecipeHandler)
@@ -116,13 +125,4 @@ func main() {
 	router.PUT("/recipes/:id", UpdateRecipeHandler)
 	router.DELETE("/recipes/:id", DeleteRecipeHandler)
 	router.Run()
-}
-
-type Recipe struct {
-	ID           string    `json:"id"`
-	Name         string    `json:"name"`
-	Tags         []string  `json:"tags"`
-	Ingredients  []string  `json:"ingredients"`
-	Instructions []string  `json:"instructions"`
-	PublishedAt  time.Time `json:"publishedAt"`
 }
